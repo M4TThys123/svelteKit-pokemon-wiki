7b323a69db35e526c8cd50ef9ede957657f77207
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-10-19 16:47:37 +0200

    🎉 Initialized Svelte repository

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script><svelte:head>
+    <title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1>Svelte Kit Pokedex</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    h1{
+        color: red;
+    }
+</style>
